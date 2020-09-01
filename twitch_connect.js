96dd2dc23da004a639e04c6adaf3414c97033d89
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2020-09-01 15:53:11 +0900

    Add experimental twitch plugin
    This supports only getting twitch json yet(more coming soon)

---
+++ twitch_connect.js
@@ -0,0 +1,44 @@
+(function () {
+  "use strict";
+  var port = 8000;
+  var socket = network.createSocket();
+  
+  function update_twitch() {
+    console.log("update_twitch");
+    if(!socket.write('get_audiences\r\n')) console.log("get_audiences ERROR!");
+  }
+
+  function main() {
+    socket.on('error', function(hadError) {
+      if(hadError) {
+        console.log("hadError true");
+      } else {
+        console.log("hadError false");
+      }
+    });
+    
+    socket.on('data', function(data) {
+      if(data.indexOf("Connected") !== -1) {
+        context.subscribe("interval.day", update_twitch);
+      } else if(data == "ping") {
+        socket.write('pong');
+      } else {
+        console.log("Received Data - " + data);
+      }
+    });
+    
+    socket.connect(port, "localhost", function() {
+      console.log('socket connected');
+      socket.write('CONNECT #lastorder_dc\r\n');
+    });
+  }
+
+  registerPlugin({
+    name: "openrct2-twitch-test",
+    version: "0.0.1",
+    licence: "MIT",
+    authors: ["Lastorder-DC"],
+    type: "remote",
+    main: main,
+  });
+})();
